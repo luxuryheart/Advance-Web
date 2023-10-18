74351ac03cbbc1be94be406f2f6851a341b5138f
luxuryheart <111441106+luxuryheart@users.noreply.github.com>
2023-10-17 22:27:26 -0600

    Update course.js

--- abc/routes/course.js
+++ abc/routes/course.js
@@ -1,12 +1,35 @@
-const express = require('express');
+var expressFunction = require("express");
+const router = expressFunction.Router();
+const auth = require("../config/authorize");
+let courses = require("../data/course_data");
+const authorization = require("../config/authorize");
 
-const router = express.Router();
-
-const course = require('../data/course_data');
-const auth = require('../config/authorize');
-
-router.get('/course', auth, (req, res) => {
-    res.send(course)
+router.get('/course', authorization, async (req, res) => {
+    res.send(courses)
 })
+
+router.post('/course', authorization, async (req, res) => {
+    const course = {
+        course_id: req.body.course_id,
+        name: req.body.name,
+        credit: req.body.credit,
+        instructor:req.body.instructor 
+    }
+    courses.push(course)
+    console.log(courses);
+
+    res.status(200).json({message: "Add course successfully", courses})
+})
+
+router.delete('/course', authorization, async (req, res) => {
+    const { index } = req.body
+    console.log(courses)
+
+    courses.splice(index, 1)
+    console.log(courses)
+
+    res.status(200).json({message: "Delete course successfully" , courses})
+})
+
 
 module.exports = router;
